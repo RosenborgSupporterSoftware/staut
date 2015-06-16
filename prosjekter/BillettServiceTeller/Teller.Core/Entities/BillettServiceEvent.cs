2e190b6df46b7238183fd824af3ef115ea5ecaa4
Rune Jacobsen <havremunken@gmail.com>
2015-06-16 13:11:52 +0200

    Legg til BillettServiceEvent og Measurement entities

---
+++ prosjekter/BillettServiceTeller/Teller.Core/Entities/BillettServiceEvent.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Teller.Core.Entities
+{
+    /// <summary>
+    /// En klasse som representerer en BillettService-"event" - i praksis en kamp for vår del
+    /// </summary>
+    public class BillettServiceEvent
+    {
+        #region Properties
+
+        /// <summary>
+        /// BillettService sin egen id på arrangementet
+        /// </summary>
+        public int EventId { get; set; }
+
+        /// <summary>
+        /// Navnet på motstanderen i kampen
+        /// </summary>
+        public string Opponent { get; set; }
+
+        /// <summary>
+        /// Starttidspunkt for kampen
+        /// </summary>
+        public DateTime Start { get; set; }
+
+        /// <summary>
+        /// URL for selve eventet - typisk siden der en vanlig bruker bestiller billetter
+        /// </summary>
+        public string EventUrl { get; set; }
+
+        /// <summary>
+        /// URL for datafila til BillettService med setedata
+        /// </summary>
+        public string AvailibilityUrl { get; set; }
+
+        /// <summary>
+        /// Vårt endelige estimat på hvor mange plasser som ble solgt
+        /// </summary>
+        public string FinalEstimatedSeatCount { get; set; }
+
+        /// <summary>
+        /// Tallet klubben rapporterer på solgte plasser
+        /// </summary>
+        public int OfficialSeatCount { get; set; }
+
+        #endregion
+
+    }
+}
---
+++ prosjekter/BillettServiceTeller/Teller.Core/Entities/Measurement.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Teller.Core.Entities
+{
+    /// <summary>
+    /// En klasse som representerer et "snapshot" av salget til en kamp
+    /// </summary>
+    public class Measurement
+    {
+        /// <summary>
+        /// Unik id for denne målingen
+        /// </summary>
+        public int MeasurementId { get; set; }
+
+        /// <summary>
+        /// Id på event som denne målingen hører til
+        /// </summary>
+        public int EventId { get; set; }
+
+        /// <summary>
+        /// Tidspunktet denne målingen ble foretatt
+        /// </summary>
+        public DateTime MeasurementTime { get; set; }
+
+        /// <summary>
+        /// Antall enkeltseter som er solgt på dette tidspunktet
+        /// </summary>
+        public int AmountSold { get; set; }
+
+        /// <summary>
+        /// Antall sesongkort som er solgt på dette tidspunktet
+        /// </summary>
+        public int AmountSeasonTicket { get; set; }
+
+        /// <summary>
+        /// Totalt antall solgte seter - summen av Sold og SeasonTicket
+        /// </summary>
+        public int TotalAmountSold { get { return AmountSold + AmountSeasonTicket; } }
+
+        /// <summary>
+        /// Seter som er tilgjengelige for salg
+        /// </summary>
+        public int AmountAvailable { get; set; }
+
+        /// <summary>
+        /// Seter som er reservert for salg
+        /// </summary>
+        public int AmountReserved { get; set; }
+
+        /// <summary>
+        /// Seter som utilgjengelige
+        /// </summary>
+        public int AmountUnavailable { get; set; }
+
+        /// <summary>
+        /// Seter holdt av i flash-applikasjonen
+        /// </summary>
+        public int AmountTicketMaster { get; set; }
+
+        /// <summary>
+        /// Seter vi ikke har en god peiling på statusen til
+        /// </summary>
+        public int AmountUnknown { get; set; }
+    }
+}
