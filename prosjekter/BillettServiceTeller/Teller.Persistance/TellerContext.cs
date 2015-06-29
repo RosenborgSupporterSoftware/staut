bdcfbcaf4e0d338a491853c47777591cfdc5b655
Rune Jacobsen <havremunken@gmail.com>
2015-06-29 09:49:14 +0200

    Create TellerContext class for DB access

---
+++ prosjekter/BillettServiceTeller/Teller.Persistance/TellerContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teller.Core.Entities;
+
+namespace Teller.Persistance
+{
+    public class TellerContext : DbContext
+    {
+        /// <summary>
+        /// Kampene vi teller
+        /// </summary>
+        public DbSet<BillettServiceEvent> Events { get; set; }
+
+        /// <summary>
+        /// Målinger gjort på kamper
+        /// </summary>
+        public DbSet<Measurement> Measurements { get; set; }
+
+    }
+}
