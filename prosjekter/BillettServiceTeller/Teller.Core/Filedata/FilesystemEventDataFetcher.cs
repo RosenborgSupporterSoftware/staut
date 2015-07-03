e2b920e5228d1ed669186a2de0051cd3da0b3fb1
Rune Jacobsen <havremunken@gmail.com>
2015-07-03 22:15:07 +0200

    Create implementation of IEventDataFetcher - FilesystemEventDataFetcher

---
+++ prosjekter/BillettServiceTeller/Teller.Core/Filedata/FilesystemEventDataFetcher.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Teller.Core.BillettService;
+using Teller.Core.Entities;
+
+namespace Teller.Core.Filedata
+{
+    public class FilesystemEventDataFetcher : IEventDataFetcher
+    {
+        private Dictionary<string, BillettServiceEvent> _events;
+
+        public IEnumerable<BillettServiceEvent> FetchEvents(string archivePath)
+        {
+            if(String.IsNullOrWhiteSpace(archivePath) || !Directory.Exists(archivePath))
+                throw new ArgumentException("Dårlig filområde");
+
+            // Loop gjennom archivePath sine subdirectories etter eventinfo.properties-filer
+            var reader = new EventInfoPropertyReader();
+
+            _events = Directory.GetFiles(archivePath, "eventinfo.properties", SearchOption.AllDirectories)
+                               .ToDictionary(file => Path.GetDirectoryName(file), file => reader.ReadProperties(file));
+
+            return _events.Values;
+        }
+
+        public IEnumerable<MeasurementFile> GetMeasurements(BillettServiceEvent bsEvent)
+        {
+            if(!_events.ContainsValue(bsEvent))
+                yield break;
+
+            var dir = _events.Single(kvp => kvp.Value == bsEvent).Key;
+
+            var files = Directory.GetFiles(dir, "*.xml", SearchOption.TopDirectoryOnly);
+
+            foreach (var file in files)
+            {
+                yield return new MeasurementFile(file);
+            }
+        }
+    }
+}
---
+++ prosjekter/BillettServiceTeller/Teller.Tests/Filedata/FilesystemEventDataFetcherTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Teller.Core.Filedata;
+using Xunit;
+
+namespace Teller.Tests.Filedata
+{
+    public class FilesystemEventDataFetcherTests
+    {
+        #region FetchEvents tests
+
+        [Fact]
+        public void FetchEvents_WhenGivenRealDataDirectory_ReturnsExpectedObjects()
+        {
+            // Arrange
+            var sut = new FilesystemEventDataFetcher();
+
+            // Act
+            var res = sut.FetchEvents(@"..\..\TestData\archive")
+                         .ToList();
+
+            // Assert
+            res.Count.Should().Be(3, "we have three directories with eventinfo.properties files");
+        }
+
+        #endregion
+
+        #region GetMeasurements tests
+
+        [Fact]
+        public void GetMeasurements_WhenGivenRealDataDirectory_ReturnsExpectedMeasurements()
+        {
+            // Arrange
+            var sut = new FilesystemEventDataFetcher();
+            var events = sut.FetchEvents(@"..\..\TestData\archive")
+                            .ToList();
+            var odd = events.Single(e => e.Opponent == "Odd");
+
+            // Act
+            var res = sut.GetMeasurements(odd)
+                         .ToList();
+
+            // Assert
+            res.Count.Should().Be(3, "there are three measurements in this directory");
+            res.All(m => m.EventId == 438523).Should().BeTrue("all measurements should return the same event id");
+            res.All(m => m.MeasurementTime.Year == 2015 && m.MeasurementTime.Month == 6 && m.MeasurementTime.Day == 30)
+               .Should()
+               .BeTrue("all measurements should have the same date");
+            res.Count(m => m.MeasurementTime.Hour == 12 && m.MeasurementTime.Minute == 12)
+               .Should()
+               .Be(1, "only one of the measurements should match");
+            res.Count(m => m.MeasurementTime.Hour == 12 && m.MeasurementTime.Minute == 42)
+               .Should()
+               .Be(1, "only one of the measurements should match");
+            res.Count(m => m.MeasurementTime.Hour == 13 && m.MeasurementTime.Minute == 12)
+               .Should()
+               .Be(1, "only one of the measurements should match");
+        }
+
+        #endregion
+    }
+}
