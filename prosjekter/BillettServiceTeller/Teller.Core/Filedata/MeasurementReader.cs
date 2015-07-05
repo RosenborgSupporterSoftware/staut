96068dd65b39d8336c2f58af30e83f1e80132194
Rune Jacobsen <havremunken@gmail.com>
2015-07-05 19:03:30 +0200

    Create IMeasurementReader implementation MeasurementReader

---
+++ prosjekter/BillettServiceTeller/Teller.Core/Filedata/MeasurementReader.cs
@@ -0,0 +1,32 @@
+﻿using Teller.Core.BillettService;
+using Teller.Core.Entities;
+
+namespace Teller.Core.Filedata
+{
+    public class MeasurementReader : IMeasurementReader
+    {
+        public Measurement ReadMeasurement(MeasurementFile measurementFile)
+        {
+            var ticketFile = BillettServiceXmlFile.LoadFile(measurementFile.FullPath);
+            var summary = new SummaryGenerator().CreateSummary(ticketFile);
+
+            Measurement measurement = new Measurement
+            {
+                MeasurementTime = measurementFile.MeasurementTime,
+                AmountSold = summary.ContainsKey(SeatStatus.Sold) ? summary[SeatStatus.Sold] : 0,
+                AmountSeasonTicket = summary.ContainsKey(SeatStatus.SeasonTicket) ? summary[SeatStatus.SeasonTicket] : 0,
+                AmountAvailable =
+                    summary.ContainsKey(SeatStatus.AvailableForPurchase) ? summary[SeatStatus.AvailableForPurchase] : 0,
+                AmountReserved = summary.ContainsKey(SeatStatus.Reserved) ? summary[SeatStatus.Reserved] : 0,
+                AmountUnavailable = summary.ContainsKey(SeatStatus.Unavailable) ? summary[SeatStatus.Unavailable] : 0,
+                AmountTicketMaster =
+                    summary.ContainsKey(SeatStatus.HeldByTicketMasterApplication)
+                        ? summary[SeatStatus.HeldByTicketMasterApplication]
+                        : 0,
+                AmountUnknown = summary.ContainsKey(SeatStatus.Unknown) ? summary[SeatStatus.Unknown] : 0
+            };
+
+            return measurement;
+        }
+    }
+}
---
+++ prosjekter/BillettServiceTeller/Teller.Tests/Filedata/MeasurementReaderTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Teller.Core.Entities;
+using Teller.Core.Filedata;
+using Xunit;
+
+namespace Teller.Tests.Filedata
+{
+    public class MeasurementReaderTests
+    {
+        #region ReadMeasurement tests
+
+        #region Good file tests
+
+        [Fact]
+        public void ReadMeasurement_WhenGivenGoodFile_ReturnsExpectedData()
+        {
+            // Arrange
+            const string inputFile = @"..\..\TestData\archive\2015_LEAGUE_8_Odd_438523\438523_Odd_2015-06-30T12-12.xml";
+            var measurementFile = new MeasurementFile(inputFile);
+            var sut = new MeasurementReader();
+
+            // Act
+            var res = sut.ReadMeasurement(measurementFile);
+
+            // Assert
+            res.AmountSold.Should().Be(1352);
+            res.AmountSeasonTicket.Should().Be(8098);
+            res.AmountAvailable.Should().Be(9201);
+            res.AmountReserved.Should().Be(2933);
+            res.AmountUnavailable.Should().Be(0);
+            res.AmountTicketMaster.Should().Be(0);
+            res.AmountUnknown.Should().Be(61);
+        }
+
+
+        #endregion
+
+        #endregion
+    }
+}
