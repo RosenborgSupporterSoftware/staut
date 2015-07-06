e73424921e5f961d6dd6468f6af30f5427a23b32
Rune Jacobsen <havremunken@gmail.com>
2015-07-06 11:35:14 +0200

    Create IFileArchiver interface

---
+++ prosjekter/BillettServiceTeller/Teller.Core/Filedata/IFileArchiver.cs
@@ -0,0 +1,18 @@
+﻿using Teller.Core.Entities;
+
+namespace Teller.Core.Filedata
+{
+    public interface IFileArchiver
+    {
+        /// <summary>
+        /// Flytt én enkelt MeasurementFile til arkivet
+        /// </summary>
+        /// <param name="measurementFile">Fila som skal arkiveres</param>
+        void MoveToArchive(MeasurementFile measurementFile);
+
+        /// <summary>
+        /// Utfører diverse oppgaver for å holde arkivet ryddig, som f.eks. zipping av filer.
+        /// </summary>
+        void PerformCleanup();
+    }
+}
