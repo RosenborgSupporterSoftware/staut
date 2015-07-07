12361fc2bbe17cb72bb7c980831631a8ea359725
Rune Jacobsen <havremunken@gmail.com>
2015-07-07 14:59:53 +0200

    Add ICleanupOperation interface

---
+++ prosjekter/BillettServiceTeller/Teller.Core/Filedata/ICleanupOperation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teller.Core.Filedata
+{
+    /// <summary>
+    /// Overengineering 101 - et interface for diverse tiltak vi gjør for å rydde opp i filstrukturen
+    /// </summary>
+    public interface ICleanupOperation
+    {
+        /// <summary>
+        /// Prioritet - rekkefølge som operasjonene kjøres i. Lavere = tidligere
+        /// </summary>
+        double Priority { get; }
+
+        /// <summary>
+        /// Utfør selve cleanup-jobben
+        /// </summary>
+        void PerformCleanup();
+    }
+}
