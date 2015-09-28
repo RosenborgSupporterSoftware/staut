308538fa339e1ba0c76c49aaed749868200be019
Rune Jacobsen <havremunken@gmail.com>
2015-09-28 06:58:44 +0200

    Fill in viewmodel object with functionality

--- prosjekter/BillettServiceTeller/Teller.Charts/Viewmodels/SingleMatchViewmodel.cs
+++ prosjekter/BillettServiceTeller/Teller.Charts/Viewmodels/SingleMatchViewmodel.cs
@@ -1,18 +1,162 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using Teller.Core.Entities;
+using Teller.Core.Infrastructure;
 
 namespace Teller.Charts.Viewmodels
 {
     public class SingleMatchViewmodel : BaseViewModel
     {
-        public string Title { get { return "Rosenborg - molde"; }}
+        #region Fields
 
+        private DateTime _matchStart;
+
+        #endregion
+
+        #region Public properties
+
+        public DateTime LastUpdateTime { get; set; }
+        public string Opponent { get; private set; }
+        public string Tournament { get; private set; }
+
+        public string MatchStart
+        {
+            get
+            {
+                var noDtFormat = new CultureInfo("no", false).DateTimeFormat;
+
+                return String.Format("{0} {1}/{2}-{3} kl. {4}",
+                    UppercaseFirst(noDtFormat.GetDayName(_matchStart.DayOfWeek)),
+                    _matchStart.Day,
+                    _matchStart.Month,
+                    _matchStart.Year,
+                    _matchStart.ToString(noDtFormat.ShortTimePattern));
+            }
+        }
+
+        public string Location { get; private set; }
+        public string EnemyLogoPath { get; private set; }
+
+        public string UpdateTime 
+        { 
+            get
+            {
+                var now = LastUpdateTime; // DateTime.Now
+                return String.Format("Siste måling {0}/{1} kl. {2:00}:{3:00}", now.Day, now.Month, now.Hour, now.Minute);
+            }  
+        }
+
+        public int SoldTickets { get; set; }
+        public int SoldSeasonTickets { get; set; }
+        public int ReservedUnknownTickets { get; set; }
+        public int TicketsForSale { get; set; }
+        public int GrandSumTotal { get { return SoldTickets + SoldSeasonTickets; } }
+
+        public ObservableCollection<StautSeries> DataSeries { get; set; }
+
+        #endregion
+
+        /// <summary>
+        /// Constructor som kun blir brukt av Visual Studio i design mode - for å ha litt data.
+        /// </summary>
         public SingleMatchViewmodel()
         {
+            Opponent = "Molde";
+            Tournament = "Tippeligaen 2015";
+            _matchStart = new DateTime(2015, 6, 27, 20, 00, 00);
+            Location = "Lerkendal Stadion";
+            EnemyLogoPath = @"D:\temp\staut\logo\Molde.png";
+
+            SoldTickets = 10000;
+            SoldSeasonTickets = 8100;
+            ReservedUnknownTickets = 4800;
+            TicketsForSale = 17;
+
+            LastUpdateTime = new DateTime(2015, 06, 27, 20, 00, 00).Subtract(new TimeSpan(30, 0, 0, 0));
+
+            DataSeries = new ObservableCollection<StautSeries>
+            {
+                new StautSeries
+                {
+                    Title = "Rosenborg - molde",
+                    Start = new DateTime(2015, 06, 27, 20, 00, 00),
+                    Points = new List<StautPoint>
+                    {
+                        new StautPoint {XValue = 30, YValue = 8100},
+                        new StautPoint {XValue = 0, YValue = 21404}
+                    }
+                }
+            };
+        }
+
+        public SingleMatchViewmodel(BillettServiceEvent bsEvent)
+        {
+            Opponent = bsEvent.Opponent;
+            if (bsEvent.Tournament == "LEAGUE")
+                Tournament = "Tippeligaen 2015";
+            else if (bsEvent.Tournament == "NM")
+                Tournament = "Norgesmesterskapet 2015";
+            else if (bsEvent.Tournament == "EC")
+                Tournament = "Europacup";
+
+            Location = bsEvent.Location == "TLD" ? "Lerkendal Stadion" : "";
+            _matchStart = bsEvent.Start;
+            
+            var logoPath = StautConfiguration.Current.LogoDirectory;
+            EnemyLogoPath = Path.Combine(logoPath, Opponent + ".png");
+            if (!File.Exists(EnemyLogoPath))
+                EnemyLogoPath = Path.Combine(logoPath, "NoLogo.png");
+
+            var lastMeasurement = bsEvent.Measurements.LastOrDefault();
+            if (lastMeasurement != null)
+            {
+                SoldTickets = lastMeasurement.AmountSold;
+                SoldSeasonTickets = lastMeasurement.AmountSeasonTicket;
+                ReservedUnknownTickets = lastMeasurement.AmountReserved + lastMeasurement.AmountUnknown;
+                TicketsForSale = lastMeasurement.AmountAvailable;   
+            }
+
+            LastUpdateTime = bsEvent.Measurements
+                                    .OrderByDescending(m => m.MeasurementTime)
+                                    .First()
+                                    .MeasurementTime;
+
+            DataSeries = new ObservableCollection<StautSeries>
+            {
+                new StautSeries
+                {
+                    Title = "Rosenborg - " + bsEvent.Opponent,
+                    Start = bsEvent.Start,
+                    Points =
+                        new List<StautPoint>(
+                            bsEvent.Measurements
+                                   .Where(m => m.TotalAmountSold > 0)
+                                   .Select(
+                                m =>
+                                    new StautPoint
+                                    {
+                                        XValue = bsEvent.Start.Subtract(m.MeasurementTime).TotalDays,
+                                        YValue = m.TotalAmountSold
+                                    }))
+                }
+            };
         }
 
         public void UpdateAll()
         {
             OnPropertyChanged(String.Empty);
+        }
+
+        static string UppercaseFirst(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return string.Empty;
+
+            return char.ToUpper(s[0]) + s.Substring(1);
         }
     }
 }
