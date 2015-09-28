623e3b2be6ae0c1fe247ed92af09f6477d67a243
Rune Jacobsen <havremunken@gmail.com>
2015-09-28 06:59:20 +0200

    Make Ingest read data properly, and render charts

--- prosjekter/BillettServiceTeller/Ingest/Program.cs
+++ prosjekter/BillettServiceTeller/Ingest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Teller.Charts;
 using Teller.Core.Filedata;
 using Teller.Core.Ingestion;
@@ -13,21 +14,33 @@ namespace Ingest
         static void Main(string[] args)
         {
             // Ikke akkurat dependency injection
-            //using (var context = new TellerContext())
-            //{
-            //    var eventRepo = new EventRepository(context);
-            //    var measurementRepo = new MeasurementRepository(context);
-            //    var eventFetcher = new FilesystemEventDataFetcher();
-            //    var measurementReader = new MeasurementReader();
-            //    var fileArchiver = new FileArchiver();
+            using (var context = new TellerContext())
+            {
+                var eventRepo = new EventRepository(context);
+                var measurementRepo = new MeasurementRepository(context);
+                var eventFetcher = new FilesystemEventDataFetcher();
+                var measurementReader = new MeasurementReader();
+                var fileArchiver = new FileArchiver();
 
-            //    var ingestor = new MeasurementIngestor(eventRepo, measurementRepo, eventFetcher, measurementReader, fileArchiver);
+                var ingestor = new MeasurementIngestor(eventRepo, measurementRepo, eventFetcher, measurementReader, fileArchiver);
 
-            //    ingestor.ReadAndIngestData();
-            //}
+                ingestor.ReadAndIngestData();
+            }
 
-            var test = new RenderTest();
-            test.Render();
+            using (var context = new TellerContext())
+            {
+                var eventRepo = new EventRepository(context);
+
+                var allEvents = eventRepo.GetAll()
+                                         .ToList();
+
+                var test = new RenderTest();
+
+                foreach (var bsEvent in allEvents)
+                {
+                    test.Render(bsEvent);
+                }
+            }
         }
     }
 }
