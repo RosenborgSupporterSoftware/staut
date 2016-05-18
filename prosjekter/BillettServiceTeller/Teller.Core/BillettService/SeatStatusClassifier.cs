ec18f1e2cf0c4dd151cabc55109a0fa8dd170642
Rune Jacobsen <havremunken@gmail.com>
2016-05-18 20:30:42 +0200

    Modifiser kodeklassifisering til å bruke Juba-metoden

--- prosjekter/BillettServiceTeller/Teller.Core/BillettService/SeatStatusClassifier.cs
+++ prosjekter/BillettServiceTeller/Teller.Core/BillettService/SeatStatusClassifier.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Teller.Core.Entities;
 
 namespace Teller.Core.BillettService
@@ -41,6 +42,12 @@ namespace Teller.Core.BillettService
 
         public static bool IsKnownSeasonTicketCode(EttCode code)
         {
+            var juba = code.JubaFlags;
+
+            return juba == 9 || juba == 10 || juba == 11;
+
+            // Pre-Juba
+
             var knownSeasonCodes = new List<string>
             {
                 // Liste hentet ut ved å finne koder 0x400 - 0x600 fra to siste kamper 2015 før de er lagt ut for salg
@@ -101,6 +108,12 @@ namespace Teller.Core.BillettService
 
         public static bool IsKnownSoldCode(EttCode code)
         {
+            var juba = code.JubaFlags;
+
+            return juba == 12 || juba == 8;
+
+            // Pre-juba
+
             if (code.QualifierBitsHex == "414")
                 return true;
             if (code.QualifierBitsHex == "5C2") // Solgt basert på antagelser fra vegardj
