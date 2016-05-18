0b072202dbb094a56d78e27e9d6351653d8477cc
Rune Jacobsen <havremunken@gmail.com>
2016-05-18 20:20:53 +0200

    Få på plass en del kode for å håndtere chat

---
+++ prosjekter/BillettServiceTeller/Teller.Core/Entities/ChatMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Teller.Core.Entities
+{
+    /// <summary>
+    /// En chat-melding i web-interfacet
+    /// </summary>
+    public class ChatMessage
+    {
+        public int Id { get; set; }
+        public string SenderNickname { get; set; }
+        public DateTime Time { get; set; }
+        public string Message { get; set; }
+    }
+}
