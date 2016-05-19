da1e71f3fbaa2fa4a6f1ea02444c80b7481df249
Rune Jacobsen <havremunken@gmail.com>
2016-05-19 10:39:26 +0200

    Legg til PowerSTAut-bibliotek for Powershell Cmdlets for STAut

---
+++ prosjekter/BillettServiceTeller/PowerSTAut/FilterSeats.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+using Teller.Core.BillettService;
+using Teller.Core.Entities;
+
+namespace PowerSTAut
+{
+    [Cmdlet(VerbsCommon.Get, "FilteredSeats")]
+    public class FilterSeats : PSCmdlet
+    {
+        #region Fields
+
+        private string _filter;
+        private BillettServiceSete _sete;
+
+        #endregion
+
+        #region Parameters
+
+        [Parameter, ValidateNotNullOrEmpty]
+        public string Filter
+        {
+            get { return _filter; }
+            set { _filter = value; }
+        }
+
+        [Parameter(ValueFromPipeline = true, ValueFromRemainingArguments = true)]
+        public BillettServiceSete Seat
+        {
+            get { return _sete; }
+            set { _sete = value; }
+        }
+
+        #endregion
+
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+
+            var pos = new StadiumPosition(Seat.SectionName, Seat.RowName, Seat.SeatName);
+            if (pos.IsMatch(Filter))
+                WriteObject(Seat);
+        }
+    }
+}
---
+++ prosjekter/BillettServiceTeller/PowerSTAut/ReadSeats.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Management.Automation;
+using Teller.Core.BillettService;
+
+namespace PowerSTAut
+{
+    [Cmdlet(VerbsCommunications.Read, "Seats")]
+    public class ReadSeats : PSCmdlet
+    {
+        #region Parameters
+
+        [Parameter(Position = 0, Mandatory = true)]
+        public string Filename { get { return _filename; } set { _filename = value; } }
+        private string _filename;
+
+        #endregion
+
+        #region Cmdlet functionality
+
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+
+            var reader = new BillettServiceSeteLeser();
+            var file = BillettServiceXmlFile.LoadFile(_filename);
+            var seats = reader.ReadSeats(file).ToList();
+
+            // TODO: Do the thing with the stuff.
+            WriteVerbose("Antall seter: " + seats.Count);
+
+            try
+            {
+                WriteObject(seats, true);
+            }
+            catch (PipelineStoppedException e)
+            {
+                //WriteVerbose("Got exception while writing seats to stream: " + e.Message);
+            }
+        }
+
+        #endregion
+    }
+}
