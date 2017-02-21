20f2eccceda45a0afa69951f64a5909181616566
Rune Jacobsen <havremunken@gmail.com>
2017-02-21 06:45:08 +0100

    Start working on removing static AutoMapper config

--- prosjekter/BillettServiceTeller/StautApi/Global.asax.cs
+++ prosjekter/BillettServiceTeller/StautApi/Global.asax.cs
@@ -1,15 +1,20 @@
 ﻿using System.Web;
 using System.Web.Http;
+using AutoMapper;
 
 namespace StautApi
 {
     public class WebApiApplication : HttpApplication
     {
+        private MapperConfiguration _config;
+
+        public MapperConfiguration MapperConfiguration => _config;
+
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
             GlobalConfiguration.Configure(IocConfig.Bootstrap);
-            MapperConfig.Configure();
+            _config = MapperConfig.Configure();
         }
     }
 }
