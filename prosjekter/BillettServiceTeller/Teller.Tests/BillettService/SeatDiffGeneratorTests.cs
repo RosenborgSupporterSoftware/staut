e016f3256a2740dbb14022ee6aebe66f90665db3
Rune Jacobsen <havremunken@gmail.com>
2017-02-23 18:05:07 +0100

    Fix file reference in SeatDiffGeneratorTests

--- prosjekter/BillettServiceTeller/Teller.Tests/BillettService/SeatDiffGeneratorTests.cs
+++ prosjekter/BillettServiceTeller/Teller.Tests/BillettService/SeatDiffGeneratorTests.cs
@@ -14,8 +14,8 @@ namespace Teller.Tests.BillettService
         {
             // Arrange
             var sut = new SeatDiffGenerator();
-            var fileA = @"D:\temp\staut\archive\til\476973_TromsÃ¸_2016-08-16T10-48.xml";
-            var fileB = @"D:\temp\staut\archive\til\476973_TromsÃ¸_2016-08-16T11-28.xml";
+            var fileA = @"..\..\TestData\archive\til\476973_TromsÃ¸_2016-08-16T10-48.xml";
+            var fileB = @"..\..\TestData\archive\til\476973_TromsÃ¸_2016-08-16T11-28.xml";
             
             // Act
             var res = sut.GenerateDiff(fileA, fileB)
