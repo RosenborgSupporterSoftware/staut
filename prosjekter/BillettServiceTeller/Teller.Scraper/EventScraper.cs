3994ac7b3dfc29e281fd762db52c6df6d668ac9a
Rune Jacobsen <havremunken@gmail.com>
2017-03-16 19:55:41 +0100

    Add scraper project and convert project to VS 2017

---
+++ prosjekter/BillettServiceTeller/Teller.Scraper/EventScraper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using ScrapySharp.Extensions;
+using ScrapySharp.Network;
+
+namespace Teller.Scraper
+{
+    public class EventScraper
+    {
+        public IEnumerable<WebEventDetails> Scrape()
+        {
+            var browser = new ScrapingBrowser();
+
+            var resultsPage = browser.NavigateToPage(new Uri("http://www.ticketmaster.no/venue/lerkendal-stadion-trondheim-billetter/tld/25"));
+
+            var items = resultsPage.Html.CssSelect("div#event-lists > div#national-events > div > ul.table__row--event");
+
+            foreach (var htmlNode in items)
+            {
+                Console.WriteLine(htmlNode.InnerText);
+            }
+
+            return null;
+        }
+
+    }
+}
---
+++ prosjekter/BillettServiceTeller/Teller.Tests/Scraper/EventScraperTestsTemp.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Teller.Scraper;
+using Xunit;
+
+namespace Teller.Tests.Scraper
+{
+    public class EventScraperTestsTemp
+    {
+        [Fact]
+        public void TestScraping()
+        {
+            var sut = new EventScraper();
+
+            var res = sut.Scrape().ToList();
+        }
+    }
+}
